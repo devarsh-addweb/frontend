959a36784ae280847c50756ffd162b23808f0455
devarsh-addweb <devarsh.addweb@gmail.com>
2021-11-01 18:28:38 +0530

    major project

---
+++ t3_big_ecommerce/footer.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/custom.css">
+    <!-- star link-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- end-->
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Klee+One:wght@600&family=Quicksand:wght@300;400&display=swap" rel="stylesheet"> 
+    <style>
+    body 
+    {
+        background-color: white;
+    }
+    .navlink{
+      text-decoration:none;
+      color: grey;
+    }
+    a.navlink:hover{
+      color: white;
+    }
+    </style>
+</head>
+<div class="container-fluid p-5" style="background-color: black;">
+    <div class="container">
+        <div class="row" >
+              <div class="col-4 " style="">
+                <div class="row">
+                  <font style="border:none; font-size:19px; color: white;"><b>Dounload Our App</b></font>
+                </div>
+                <div class="row">
+                  <font style="border:none; font-size:14px; color: grey;">Download App for Android and ios mobile phone.</font>
+                </div><br>
+                <div class="row">
+                   <img src="img/play1.png" style="width: 35%;">
+                  <img src="img/play2.png" style="width: 35%;">
+                </div>
+              </div>  
+
+
+              <div class="col-4 " style="">
+                <div class="row">
+                  <font class="bdr" style="font-size:19px; margin-left: 150px; color: white; width: 115px; border: 1px solid white;">BackBone</font>
+                </div>
+                <div class="row">
+                  
+                </div><br><br>
+                <div class="row">
+                  <font style="border:none;  margin-top: -20px; margin-left: -20px; font-size:14px; color:grey;">Our Purpose Is To Sustainably Make th Pleasure and Benefits of Sports Accessible to the Many.</font>
+                </div>
+              </div>  
+
+
+              <div class="col-2" style="margin-right:0px; ">
+                <div class="row">
+                  <font style="border:none; font-size:19px; color: white;"><b>Useful Links</b></font>
+                </div>
+                <div class="row">
+                  <font style="border:none; font-size:14px; color: white; float: right;">
+                    <a  class="navlink" href="">Coupons</a><br>
+                    <a class="navlink" href="">Blog Post</a><br>
+                    <a class="navlink" href="">return Policy</a><br>
+                  <a class="navlink" href="">Join Affiliate</a></font>
+                </div><br>
+              </div>  
+
+
+             <div class="col-2" style="margin-right:px ;">
+                <div class="row">
+                  <font style="border:none; font-size:19px; color: white;"><b>Follow us</b></font>
+                </div>
+                <div class="row">
+                  <font style="border:none; font-size:14px; color: white; float: right;">
+                    <a  class="navlink" href="http://instagram.com/_u/devarshhh_22/">Instgram</a><br>
+                    <a class="navlink" href="https://www.facebook.com/Devarsh chuahan">Facebook</a><br>
+                    <a class="navlink" href="https://www.youtube.com/">You Tube</a><br>
+                  <a  class="navlink" href="https://twitter.com/dev_negative">Twitter</a></font>
+                </div><br>
+              </div> <br>
+              <hr style="color: white; height: 4px;">
+              <center><font style="color:white; font-size:20px">Copytight 2021 - Devarsh Chauhan</font></center>
+        </div>
+      </div>
+  </div>
+</div>
+
+ 
+
+  
